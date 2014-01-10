bd8638ba13f1c45c54d4fdc632197f7997f84deb
Rob <basicer@basicer.com>
2014-01-10 03:04:16 -0500

    - Improve lappend and make it actually save back to the list.

--- src/main/java/com/basicer/parchment/tcl/LAppend.java
+++ src/main/java/com/basicer/parchment/tcl/LAppend.java
@@ -7,6 +7,8 @@ import com.basicer.parchment.TCLEngine;
 import com.basicer.parchment.parameters.ListParameter;
 import com.basicer.parchment.parameters.Parameter;
 
+import java.util.ArrayList;
+
 public class LAppend extends TCLCommand {
 
 	
@@ -23,12 +25,17 @@ public class LAppend extends TCLCommand {
 		Parameter p = Set.access(varname, false, null, ctx.up(1));
 		if ( p == null ) return EvaluationResult.makeError("Couldnt find variable: " + varname); 
 		ListParameter list = p.cast(ListParameter.class);
-		if ( list == null ) return EvaluationResult.makeError("Not a list?!");
+		if ( list == null ) {
+			ArrayList<Parameter> x = new ArrayList<Parameter>();
+			x.add(p);
+			list = ListParameter.from(x);
+		}
 		for ( int i = 0; i < argc; ++i ) {
 			list.add(ctx.getArgs().get(i));
 			
 		}
-		
+
+		Set.access(varname, true, list, ctx.up(1));
 		return new EvaluationResult(list);
 	}
 
